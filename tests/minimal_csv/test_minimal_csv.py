7ca1dc379816f38910bcb2373a5fc969a6a6a56b
Blaise Thompson <blaise@untzag.com>
2023-06-08 14:23:18 -0500

    pandas dependency

--- tests/minimal_csv/test_minimal_csv.py
+++ tests/minimal_csv/test_minimal_csv.py
@@ -6,6 +6,6 @@ __here__ = pathlib.Path(__file__).parent
 
 
 def test_run():
-    path = __here__ / "rxn_control_input.csv"
+    path = __here__ / "recipe.csv"
     recipe = auto_rxn.Recipe(path)
     auto_rxn.run(recipe)
