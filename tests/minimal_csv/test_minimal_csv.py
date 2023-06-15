eb59c398cba1c6f9989d3fc5d5595f658dc1bfa5
Blaise Thompson <blaise@untzag.com>
2023-06-15 16:41:07 -0500

    happi db decorator

--- tests/minimal_csv/test_minimal_csv.py
+++ tests/minimal_csv/test_minimal_csv.py
@@ -1,10 +1,14 @@
 import pathlib
+import os
+
 import auto_rxn
 
 
 __here__ = pathlib.Path(__file__).parent
+happi_db = __here__ / "db.json"
 
 
+@auto_rxn.with_happi_db(happi_db)
 def test_run():
     path = __here__ / "recipe.csv"
     recipe = auto_rxn.Recipe(path)
