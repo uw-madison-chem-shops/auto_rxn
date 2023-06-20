e469a4f218e0fdd1176a0f45018abc7c8bfcdc2d
Blaise Thompson <blaise@untzag.com>
2023-06-20 10:43:39 -0500

    using bluesky, actually run (#11)
    * using bluesky, actually run
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * pytest setuptools 67
    
    * <67
    
    * switch order of install
    
    * 67.8.0
    
    * no build isolation
    
    * no build isolation
    
    * install deps
    
    * install deps
    
    * flit
    
    * matplotlib
    
    * mypy no build isolation
    
    * type: ignore
    
    * relax from reading to dict
    
    * entry points runner no build isolation
    
    * wat
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- auto_rxn/_run.py
+++ auto_rxn/_run.py
@@ -1,6 +1,14 @@
 __all__ = ["run"]
 
 
+import functools
+import pathlib
+
+import bluesky
+from bluesky.callbacks.best_effort import BestEffortCallback
+from suitcase.csv import Serializer  # type: ignore
+
+
 def run(recipe):
     from ._happi import happi_client
 
@@ -8,3 +16,26 @@ def run(recipe):
     for id in recipe.control_point_ids:
         device = happi_client.load_device(name=id)
         devices[id] = device
+    all_devices = list(devices.values())
+
+    def plan():
+        @bluesky.preprocessors.stage_decorator(all_devices)
+        @bluesky.preprocessors.run_decorator()
+        def inner_plan():
+            for step in recipe.steps:
+                for id, val in step.setpoints.items():
+                    yield from bluesky.plan_stubs.mv(devices[id], float(val))
+                yield from bluesky.plan_stubs.repeat(
+                    functools.partial(bluesky.plan_stubs.one_shot, all_devices),
+                    num=int(float(step.length) * 60),
+                    delay=1,
+                )
+
+        return (yield from inner_plan())
+
+    RE = bluesky.RunEngine()
+    bec = BestEffortCallback()
+    bec.disable_plots()
+    RE.subscribe(bec)
+    RE.subscribe(Serializer("~/auto-rxn-data/", flush=True))
+    RE(plan())
