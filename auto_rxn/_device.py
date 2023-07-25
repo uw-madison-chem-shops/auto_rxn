ae831692e3429c28dcdf84a51b3d28a61f467264
Blaise Thompson <blaise@untzag.com>
2023-07-25 16:54:54 -0500

    actually check safety limits

--- auto_rxn/_device.py
+++ auto_rxn/_device.py
@@ -20,17 +20,17 @@ def load_device(id):
     item = happi_client.find_item(name=id)
     setattr(device, "_happi_item", item)
     # limits
-    if not hasattr(item, "auto_rxn_lower_safety_limit"):
+    if not "auto_rxn_lower_safety_limit" in item.keys():
         item.info_names.append("auto_rxn_lower_safety_limit")
-        item.auto_rxn_lower_safety_limit = None
-    if not hasattr(item, "auto_rxn_upper_safety_limit"):
+        item["auto_rxn_lower_safety_limit"] = None
+    if not "auto_rxn_upper_safety_limit" in item.keys():
         item.info_names.append("auto_rxn_upper_safety_limit")
-        item.auto_rxn_upper_safety_limit = None
+        item["auto_rxn_upper_safety_limit"] = None
     if hasattr(device, "yaq_traits"):
         if "has-limits" in device.yaq_traits:
             lower, upper = device.yaq_client.get_limits()
-            item.auto_rxn_lower_safety_limit = np.nanmax(lower, item.auto_rxn_lower_safety_limit)
-            item.auto_rxn_upper_safety_limit = np.nanmin(upper, item.auto_rxn_upper_safety_limit)
+            item["auto_rxn_lower_safety_limit"] = np.nanmax(lower, item.auto_rxn_lower_safety_limit)
+            item["auto_rxn_upper_safety_limit"] = np.nanmin(upper, item.auto_rxn_upper_safety_limit)
     # finish
     item.save()
     return device
--- auto_rxn/_safety.py
+++ auto_rxn/_safety.py
@@ -5,13 +5,26 @@ import functools
 
 
 class SafetyCallback(object):
-    def __init__(self, devices):
+    def __init__(self, devices: dict):
         self.devices = devices
         self.current_step = None
         self.i = 0
 
     def __call__(self, name, document):
         """Consumes the document stream."""
-        self.i += 1
-        if self.i == 6:
-            raise Exception
+        if "data" not in document:
+            return
+        data = document["data"]
+        for name, device in self.devices.items():
+            for suffix in ["setpoint", "readback"]:
+                key = "_".join([name, "readback"])
+                if key not in data:
+                    return
+                if device._happi_item["auto_rxn_lower_safety_limit"] is not None:
+                    if data[key] < device._happi_item["auto_rxn_lower_safety_limit"]:
+                        print(f"{name} went below safety limit!")
+                        raise Exception
+                if device._happi_item["auto_rxn_upper_safety_limit"] is not None:
+                    if data[key] > device._happi_item["auto_rxn_upper_safety_limit"]:
+                        print(f"{name} went above safety limit!")
+                        raise Exception
