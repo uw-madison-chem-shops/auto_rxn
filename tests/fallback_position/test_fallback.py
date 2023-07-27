2f50cc3a26b5299c8853e30edfe8598d9d37c848
Blaise Thompson <blaise@untzag.com>
2023-07-27 16:28:56 -0500

    actually test fallback behavior...

--- tests/fallback_position/test_fallback.py
+++ tests/fallback_position/test_fallback.py
@@ -11,17 +11,13 @@ happi_db = __here__ / "db.json"
 
 
 @auto_rxn.with_happi_db(happi_db)
-def test_ramp_time_called():
+def test_fallback():
     furnace = auto_rxn.load_device("myfurnace")
-    furnace.ramp_time.set = MagicMock(side_effect=furnace.ramp_time.set)
     path = __here__ / "recipe.csv"
     recipe = auto_rxn.Recipe(path)
     auto_rxn.run(recipe)
-    assert call(0.0) == furnace.ramp_time.set.mock_calls.pop(0)
-    assert call(0.1) == furnace.ramp_time.set.mock_calls.pop(0)
-    assert call(0.2) == furnace.ramp_time.set.mock_calls.pop(0)
-    assert call(0.3) == furnace.ramp_time.set.mock_calls.pop(0)
+    assert furnace.read()["myfurnace_setpoint"]["value"] == 50.0
 
 
 if __name__ == "__main__":
-    test_ramp_time_called()
+    test_fallback()
