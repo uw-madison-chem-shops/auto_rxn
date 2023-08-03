eb39f6166c12ece0aae62f28bc03774b079d5ff0
Blaise Thompson <blaise@untzag.com>
2023-08-03 17:19:41 -0500

    handle case of partially filled out limits

--- auto_rxn/testing/_with_limit_set_to.py
+++ auto_rxn/testing/_with_limit_set_to.py
@@ -8,10 +8,15 @@ def with_limit_set_to(id, limit, value):
     def decorator(function):
         def wrapper():
             if id in auto_rxn.limits._state:
-                original = auto_rxn.limits._state[id][limit]
-                auto_rxn.limits._state[id][limit] = value
-                function()
-                auto_rxn.limits._state[id][limit] = original
+                if limit in auto_rxn.limits._state[id]:
+                    original = auto_rxn.limits._state[id][limit]
+                    auto_rxn.limits._state[id][limit] = value
+                    function()
+                    auto_rxn.limits._state[id][limit] = original
+                else:
+                    auto_rxn.limits._state[id][limit] = value
+                    function
+                    del auto_rxn.limits._state[id][limit]
             else:
                 auto_rxn.limits._state[id] = dict()
                 auto_rxn.limits._state[id][limit] = value
